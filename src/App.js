e5fa564bd87b4e184229aebf2c6d4cfcad0aa8fe
Maria Geovana Gonçalo de Oliveira <mariageovana82268@gmail.com>
2025-08-09 18:04:28 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+import './estilo.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      frase: ''
+    }
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+    this.frases = ['Siga os bons e aprenda com eles.', 
+      'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.']
+  }
+
+  quebraBiscoito() {
+    let state = this.state
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+    state.frase = '"' + this.frases[numeroAleatorio] + '"'
+    this.setState(state)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className="img"/>
+        <Botao nome="Abrir biscoito" acaoBotao={this.quebraBiscoito}/>
+        <h3 className="frase">{this.state.frase}</h3>
+      </div>
+    )
+  }
+}
+
+class Botao extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.acaoBotao}>{this.props.nome}</button>
+      </div>
+    )
+  }
+}
+
+export default App
